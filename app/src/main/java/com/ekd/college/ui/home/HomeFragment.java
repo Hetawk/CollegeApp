7244c7586dbf9a179dedfe815688062b615090c9
Enoch Kwateh Dongbo <enoch.dongbo@gmail.com>
2022-06-01 02:54:04 +0800

    google map commit

--- app/src/main/java/com/ekd/college/ui/home/HomeFragment.java
+++ app/src/main/java/com/ekd/college/ui/home/HomeFragment.java
@@ -1,23 +1,29 @@
 package com.ekd.college.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 import com.ekd.college.R;
 
+import java.net.URI;
+
 public class HomeFragment extends Fragment {
 //    private SliderLayout sliderLayout;
+    private ImageView map;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+//        return inflater.inflate(R.layout.fragment_home, container, false);
 
         //        sliderLayout = view.findViewById(R.id.slider);
 //        sliderLayout.setIndicatorAnimation(IndicatorAnimations.FILL);
@@ -27,8 +33,23 @@ public class HomeFragment extends Fragment {
 
 
 //        setSliderViews();
-//        return view;
+        map = view.findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+        });
+        return view;
 
+
+    }
+
+    private void openMap() {
+        Uri uri = Uri.parse("geo:0, 0?q=65 Florence St, Worcester, MA 01610, USA");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
     }
 
 //    private void setSliderViews() {
